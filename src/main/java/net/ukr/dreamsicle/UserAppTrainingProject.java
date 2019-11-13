9d5fc6fad37ba6ab1678c81a1e82c25b79f8763e
yurii.loienko <dreamsicle@ukr.net>
2019-11-13 09:06:10 +0200

    feature_CURR-125_Initial_project

--- src/main/java/net/ukr/dreamsicle/UserAppTrainingProject.java
+++ src/main/java/net/ukr/dreamsicle/UserAppTrainingProject.java
@@ -1,7 +1,7 @@
 package net.ukr.dreamsicle;
 
-public class App {
+public class UserAppTrainingProject {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }

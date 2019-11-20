3cf762e766dd8ac3378af52f06b753357bdf629e
yurii.loienko <dreamsicle@ukr.net>
2019-11-20 11:43:20 +0200

    feature_CURR-127_Create_API_for_entities

--- src/main/java/net/ukr/dreamsicle/service/UserService.java
+++ src/main/java/net/ukr/dreamsicle/service/UserService.java
@@ -8,12 +8,20 @@ import java.util.stream.Collectors;
 
 public class UserService {
 
-    private final UserDaoImpl user = new UserDaoImpl();
+    private static final String SORRY_USER_NOT_CREATED = "Sorry, user not created";
+    private static final String USER_SUCCESSFULLY_CREATED = "User successfully created";
+    private static final String SORRY_ROLE_NOT_DELETED = "Sorry, role not deleted";
+    private static final String ROLE_SUCCESSFULLY_DELETED = "Role successfully deleted";
+    private static final String ROLE_SUCCESSFULLY_UPDATED = "Role successfully updated";
+    private static final String SORRY_ROLE_NOT_UPDATED = "Sorry, role not updated";
+    private static final Integer IDS = 0;
+
+    private final UserDaoImpl userDao = new UserDaoImpl();
     private final UserMapper userMapper = new UserMapper();
 
     public String findAll() {
         return userMapper.findAll(
-                user.findAll())
+                userDao.findAll())
                 .stream()
                 .map(userDto -> UserDto.builder().toJson(userDto))
                 .collect(Collectors.toList())
@@ -23,8 +31,12 @@ public class UserService {
     public String findById(Integer id) {
         return UserDto.builder()
                 .toJson(userMapper.toDto(
-                        user.findById(id)
+                        userDao.findById(id)
                         )
                 );
+    }
+
+    public String create(UserDto userDto) {
+        return userDao.create(userMapper.fromDto(userDto)).equals(IDS) ? SORRY_USER_NOT_CREATED : USER_SUCCESSFULLY_CREATED;
     }
 }
